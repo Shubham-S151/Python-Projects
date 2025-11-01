3255e109ee2828d4862fb29158d47a679570a2f9
Shubham <shubhamsharma15104@gmail.com>
2025-11-01 12:08:27 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as num
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-a=[1000,500,678,456,389,200,567,300,450,564,100,567,300,500]
-print(num.mean(a))
-
